0576a03a3a7dff009ce1e2b43ba3c5dbc881a989
ehdgur3125 <ehdgur3125@naver.com>
2021-03-23 15:43:52 +0900

    [main/hotIssue]/fix date function in where clause in hotIssue #25

--- controllers/main/hotIssue.js
+++ controllers/main/hotIssue.js
@@ -2,22 +2,19 @@ const model=require("../../models");
 const jwt=require('jsonwebtoken');
 
 module.exports=async function(req,res,date){
-  let dateObj;
   if(!date){
     const tmp=new Date();
-    dateObj=new Date(`${tmp.getFullYear()}-${tmp.getMonth()}-${tmp.getDate()}`);
+    date=`${tmp.getFullYear()}-${tmp.getMonth()}-${tmp.getDate()}`;
   }
   else{
     try{
-      dateObj=new Date(date);
+      new Date(date);
     }
     catch(e){
       res.status(400).send(`${date}is not date`);
       return;
     }
   }
-  const nextDateObj=new Date(dateObj.getTime()+(24*60*60*1000));
-  console.log(dateObj, nextDateObj);
   const smallIssues=await model.vote.findAll({
     attributes:[
       [model.Sequelize.fn('COUNT',model.Sequelize.col('vote')),'cnt']
@@ -28,14 +25,7 @@ module.exports=async function(req,res,date){
       right:true,
       attributes:['title','id',"userId"]
     },
-    where:{
-      "$post.userId$":{
-        [model.Sequelize.Op.ne]:1
-      },
-      "$post.createdAt$":{
-        [model.Sequelize.Op.in]:[dateObj,nextDateObj]
-      }
-    },
+    where:model.Sequelize.where(model.Sequelize.fn("DATE",model.Sequelize.col('post.createdAt')),date),
     group:'post.id',
     order:[[model.Sequelize.literal('cnt'),'desc'],[model.Sequelize.literal('post.id'),'desc']],
     limit:3
